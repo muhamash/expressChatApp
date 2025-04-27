104b09c9a9575898f4b915311d10d9ba32ed2d10
muhamsh <muhammad-ashraful@outlook.com>
2025-04-28 00:05:32 +0600

    user model and get user up

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,14 +1,31 @@
-function getUsers(req, res, next) {
-    if (req.accepts('html')) {
-        res.render( 'users');
-    }
-    else if (req.accepts('json')) {
-        res.json({
-            message: "This route is meant for HTML form Users.",
-        });
-    }
-    else {
-        res.type('txt').send('This route is for Users.');
+const bycrypt = require( 'bcrypt' );
+const { unlink } = require( 'fs' );
+const path = require( 'path' );
+const { User } = require( '../models/users' );
+
+async function getUsers ( req, res, next )
+{
+    try
+    {
+        const users = await User.find();
+        console.log( users );
+
+        if (req.accepts('html')) {
+            res.render( 'users', { users });
+        }
+        else if (req.accepts('json')) {
+            res.json({
+                message: "This route is meant for Users.",
+                users,
+            });
+        }
+        else {
+            res.type( 'txt' ).send( `This route is for Users`, JSON.stringify( users ) );
+        }
+
+    } catch ( error )
+    {
+        next( error );
     }
 }
 
