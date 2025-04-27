362a413e0d4806aab5fbad39901a44468dc75f32
muhamsh <muhammad-ashraful@outlook.com>
2025-04-28 00:58:25 +0600

    get add delete user up

--- controller/usersController.js
+++ controller/usersController.js
@@ -29,6 +29,109 @@ async function getUsers ( req, res, next )
     }
 }
 
+async function addUser ( req, res, next )
+{
+    let newUser;
+    const hashedPassword = await bcrypt.hash( req.body.password, 10 );
+
+    if (req.files && req.files.length > 0) {
+        newUser = new User( {
+            ...req.body,
+            avatar: req.files[ 0 ]?.filename || "",
+            password: hashedPassword,
+        } );
+    }
+    else
+    {
+        newUser = new User( {
+            ...req.body,
+            password: hashedPassword,
+        } );
+    };
+
+    try
+    {
+        const result = await newUser.save();
+
+        if (req.accepts('html')) {
+            const users = await User.find();
+            res.render('users', { users });
+        }
+        else if ( req.accepts( 'json' ) )
+        {
+            res.status( 200 ).json( {
+                message: "User was added successfully!",
+                data: result,
+            } );
+        }
+        else
+        {
+            res.type( 'txt' ).send( result );
+        }
+    }
+    catch ( error )
+    {
+        res.status( 500 ).json( {
+            errors: {
+                common: {
+                    message: "Unknown error occurred!",
+                },
+                message: error?.message || error?.toString(),
+            },
+        } );
+        next( error );
+    }
+};
+
+async function deleteUser ( req, res, next )
+{
+    const userId = req.param?.id;
+    try
+    {
+        const user = await User.findByIdAndDelete( { _id: userId } );
+        // console.log( user );
+
+        if (user?.avatar) {
+            unlink(
+                path.join( __dirname, `/../public/uploads/avatars/${user.avatar}` ),
+                ( err ) =>
+                {
+                    if ( err ) console.log( err );
+                }
+            );
+        };
+
+        if(req.accepts('html')) {
+            // const users = await User.find();
+            res.render( 'index' );
+        }
+        else if( req.accepts( 'json' ) )
+        {
+            res.status( 200 ).json( {
+                message: "User was deleted successfully!",
+                userId,
+            } );
+        }
+        else
+        {
+            res.type( 'txt' ).send( {message : 'user deleted', userId } );
+        }
+    }
+    catch ( error )
+    {
+        res.status( 500 ).json( {
+            errors: {
+                common: {
+                    message: "Can't delete the user!",
+                },
+                message: error?.message || error?.toString(),
+            },
+        } );
+        next( error );
+    }
+}
+
 module.exports = {
     getUsers,
+    addUser,
 }
