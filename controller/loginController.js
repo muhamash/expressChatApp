9faf9380a5863684c5695df276da681d5b8b0c4b
muhamsh <muhammad-ashraful@outlook.com>
2025-04-29 01:19:21 +0600

    working on login

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,4 +1,10 @@
-function getLogin(req, res, next) {
+const User = require( '../models/users' );
+const bcrypt = require( 'bcrypt' );
+const jwt = require( 'jsonwebtoken' );
+const createError = require( 'http-errors' );
+
+function getLogin ( req, res, next )
+{
     if (req.accepts('html')) {
         res.render( 'index');
     }
@@ -12,6 +18,73 @@ function getLogin(req, res, next) {
     }
 }
 
+async function postLogin ( req, res, next )
+{ 
+    try
+    {
+        const user = await User.findOne( { 
+            $or: { 
+                email: req.body.username, 
+                mobile: req.body.username 
+            }
+        } );
+        
+        console.log( "User: ", user );
+        if ( user && user._id )
+        {
+            const isValidPassword = await bcrypt.compare( req.body.password, user.password );
+
+            if(isValidPassword)
+            {
+                // set cookie
+                // res.cookie( 'userId', user._id, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true } );
+                // res.redirect( '/inbox' );
+                const userObject = {
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    mobile: user.mobile,
+                    role: user.role,
+                }
+
+                // token generation
+                const token = await jwt.sign( userObject, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION } );
+                console.log( "Token: ", token );
+                 
+                // set cookie
+                res.cookie( process.env.COOKIE_NAME, token, { maxAge: process.env.JWT_EXPIRATION, httpOnly: true, signed: true } );
+
+                res.locals.loggedInUser = userObject;
+
+                res.render( 'inbox' );
+            }
+            else
+            {
+                throw createError( 401, "Invalid username or password" );
+            }
+        }
+        else
+        {
+            throw createError( 401, "Login Failed!!" );
+        }
+    }
+    catch ( error )
+    {
+        console.log( error );
+        // next( error );
+        res.render( 'index',
+            {
+                data: {
+                    username: req.body.username,
+                }
+            },
+            {
+                error: error.message
+            } );
+    };
+}; 
+
 module.exports = {
     getLogin,
+    postLogin,
 }
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -1,9 +1,11 @@
 const express = require( 'express' );
-const { getLogin } = require( '../controller/loginController' );
+const { getLogin, postLogin } = require( '../controller/loginController' );
 const decorateHtml = require( '../middlewares/common/decorateHtmlRes' );
 
 const router = express.Router();
 
-router.get('/', decorateHtml("Login Page"), getLogin);
+router.get( '/', decorateHtml( "Login Page" ), getLogin );
+
+router.post( '/', postLogin );
 
 module.exports = router;
