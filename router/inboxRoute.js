b97f3b55568e3d65d2eae43493a724768ede27b8
muhamsh <muhammad-ashraful@outlook.com>
2025-05-06 01:14:26 +0600

    check login added

--- router/inboxRoute.js
+++ router/inboxRoute.js
@@ -1,9 +1,10 @@
 const express = require( 'express' );
 const { getInbox } = require( '../controller/inboxController' );
 const decorateHtml = require( '../middlewares/common/decorateHtmlRes' );
+const { checkLogin } = require( '../middlewares/common/checkLogin' );
 
 const router = express.Router();
 
-router.get( '/', decorateHtml("Inbox Page") ,getInbox );
+router.get( '/', decorateHtml( "Inbox Page" ),checkLogin, getInbox );
 
 module.exports = router;
