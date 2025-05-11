cca61cffa5a37f677aa1073e5254bd770f2210ca
muhamsh <muhammad-ashraful@outlook.com>
2025-05-11 22:47:33 +0600

    updated the ui >>>> wil resolve the bug later inshallah

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,13 +2,13 @@ const express = require( 'express' );
 const { getLogin, postLogin, logout } = require( '../controller/loginController' );
 const decorateHtml = require( '../middlewares/common/decorateHtmlRes' );
 const { loginValidators, loginValidationHandler } = require( '../middlewares/login/loginValidators' );
-const { redirectLoggedIn, redirectIfLoggedIn } = require( '../middlewares/common/checkLogin' );
+const { redirectIfLoggedIn } = require( '../middlewares/common/checkLogin' );
 
 const router = express.Router();
 
 // console.log( redirectLoggedIn );
 
-router.get( '/', decorateHtml( "Login Page" ), redirectIfLoggedIn, getLogin );
+router.get( '/', decorateHtml( "Login Page" ), getLogin );
 
 router.post( '/', decorateHtml( "Login Page" ), loginValidators, loginValidationHandler, postLogin );
 
